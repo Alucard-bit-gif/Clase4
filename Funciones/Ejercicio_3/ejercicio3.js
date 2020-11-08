0bc52c06b28fb88975593d5dbec6a8fafb25892e
Sebastián <serviciossociologicos@gmail.com>
2020-11-08 16:17:53 -0500

    Ejercicio comletado

--- Funciones/Ejercicio_3/ejercicio3.js
+++ Funciones/Ejercicio_3/ejercicio3.js
@@ -1,1 +1,33 @@
 //Funciones 3.
+
+//Declarar variables.
+
+let ahorro;
+let sueldo;
+let total;
+
+//leer variables.
+
+let sueldo = Number(prompt("Ingrese el valor de su sueldo"));
+
+//invocar funciones.
+
+let ahorroAnual = Ahorro(sueldo);
+
+//Procedimiento en funciones.
+
+function Ahorro(sueldo){
+
+ahorro = (sueldo*0.15)*4;
+return ahorro;
+}
+
+function Total(ahorro) {
+
+    Total = ahorro*12
+    return Total;
+}
+
+//imprimir
+
+alert('El ahorro anual es: ' + Total(ahorroAnual));
